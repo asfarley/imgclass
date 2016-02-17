bf83cf7be4a7de3638829deccf4a72a5a8dc15a7
Alexander Farley <alexander.s.farley@gmail.com>
2016-02-17 11:30:43 -0500

    Adding installer

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,4 @@
 class Image < ActiveRecord::Base
+  mount_uploader :url, ImageUploader
   belongs_to :image_set
 end
---
+++ installRails.sh
@@ -0,0 +1,31 @@
+sudo apt-get update
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+
+cd
+git clone git://github.com/sstephenson/rbenv.git .rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+
+rbenv install 2.2.3
+rbenv global 2.2.3
+ruby -v
+
+echo "gem: --no-ri --no-rdoc" > ~/.gemrc
+gem install bundler
+
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+gem install rails -v 4.2.4
+
+rbenv rehash
+
+rails -v
+# Rails 4.2.4
+
+sudo apt-get install mysql-server mysql-client libmysqlclient-dev
