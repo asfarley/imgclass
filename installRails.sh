ed174321a320c7f07b0209cbf3083a9302cc02bb
Alexander Farley <alexander.s.farley@gmail.com>
2016-02-18 13:30:45 -0800

    Upload form styling

--- installRails.sh
+++ installRails.sh
@@ -1,10 +1,11 @@
 sudo apt-get update
-sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+sudo apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 
 cd
 git clone git://github.com/sstephenson/rbenv.git .rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source .bashrc
 
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
@@ -25,7 +26,8 @@ gem install rails -v 4.2.4
 
 rbenv rehash
 
+bundle install
 rails -v
 # Rails 4.2.4
 
-sudo apt-get install mysql-server mysql-client libmysqlclient-dev
+sudo apt-get -y install mysql-server mysql-client libmysqlclient-dev
