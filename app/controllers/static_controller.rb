6d814fe26d90492b073fb74160913b4bb18d4e3a
Eugene Rymski <eugene.rymski@autodesk.com>
2016-09-20 01:03:06 +0300

    Redirect logged users to Image Sets page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,9 @@
 class StaticController < ApplicationController
 
   def index
+    if current_user then
+      redirect_to url_for(:controller => 'image_sets', :action=>'index')
+    end 
   end
   
 end
