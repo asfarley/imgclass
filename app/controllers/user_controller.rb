48bc194f83fc6d3dbe3c067cbca87257d08fa859
Eugene Rymski <eugene.rymski@autodesk.com>
2016-10-15 23:49:58 +0300

    Use recommended check for authentication validation

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,8 +3,8 @@ class UserController < ApplicationController
     before_filter :validate_user
 
     def validate_user
-        if ! current_user 
-        render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+        if ! user_signed_in? 
+            render file: "#{Rails.root}/public/404.html", layout: false, status: 404
         end
     end
 end
