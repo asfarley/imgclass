29920d42ba22f5929500cb9ef5ee0a330a4693ed
asfarley <alexander.s.farley@gmail.com>
2017-10-09 13:40:25 -0600

    Fix expected test result. Renaming test setup variables.

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -5,16 +5,16 @@ class ImageTest < ActiveSupport::TestCase
   def setup
     @im = Image.new()
     @im.save
-    @bb1 = { :x => 0.1, :y => 0.1, :width => 0.1, :height => 0.1, :classname => "Car" }
-    @bb2 = { :x => 0.6, :y => 0.6, :width => 0.1, :height => 0.1, :classname => "Car" }
+    @bb_car1 = { :x => 0.1, :y => 0.1, :width => 0.1, :height => 0.1, :classname => "Car" }
+    @bb_car2 = { :x => 0.6, :y => 0.6, :width => 0.1, :height => 0.1, :classname => "Car" }
     @il1 = ImageLabel.new()
     @il2 = ImageLabel.new()
     @il3 = ImageLabel.new()
     @il1.image_id = @im.id
     @il2.image_id = @im.id
     @il3.image_id = @im.id
-    @targets_list_two_cars = [@bb1, @bb2]
-    @targets_list_three_cars = [@bb1, @bb2, @bb2]
+    @targets_list_two_cars = [@bb_car1, @bb_car2]
+    @targets_list_three_cars = [@bb_car1, @bb_car2, @bb_car2]
     @targets_list_nothing = []
   end
 
@@ -67,7 +67,7 @@ class ImageTest < ActiveSupport::TestCase
     @il3.save
     il4.save
 
-    assert(@im.most_likely_bounding_boxes == targetJSON2)
+    assert(@im.most_likely_bounding_boxes == targetJSON1)
   end
 
   test "most_likely_bounding_boxes returns single target" do
@@ -81,7 +81,5 @@ class ImageTest < ActiveSupport::TestCase
 
     assert (@im.most_likely_bounding_boxes == targetJSON1)
   end
-
-
 
 end
