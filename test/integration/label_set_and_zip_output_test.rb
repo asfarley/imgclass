99a100d47056dbb1c6739005578941206d45d3cd
asfarley <alexander.s.farley@gmail.com>
2017-10-09 14:12:00 -0600

    Adding integration test stub.

---
+++ test/integration/label_set_and_zip_output_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class LabelSetAndZipOutputTest < ActionDispatch::IntegrationTest
+
+  test "A completely-labelled ImageLabelSet can generate a zipped output folder" do
+     assert false, "Test not implemented"
+  end
+
+end
