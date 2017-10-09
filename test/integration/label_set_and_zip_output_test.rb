78449a7711992686b5249d507cc5ebf3b8f5e064
asfarley <alexander.s.farley@gmail.com>
2017-10-09 17:01:13 -0600

    Starting work on final integration tests.

--- test/integration/label_set_and_zip_output_test.rb
+++ test/integration/label_set_and_zip_output_test.rb
@@ -3,7 +3,19 @@ require 'test_helper'
 class LabelSetAndZipOutputTest < ActionDispatch::IntegrationTest
 
   test "A completely-labelled ImageLabelSet can generate a zipped output folder" do
-     assert false, "Test not implemented"
+     # Attempt to generate output
+     ils = ImageLabelSet.first
+     ils.generate_output_folder_if_complete
+     # Check if output folder exists
+     expected_zipfile_name = File.join(Rails.root, "tmp", "ImageLabelSet_#{ils.id}.zip")
+     assert File.exist?(expected_zipfile_name), "Output zipfile does not exist after attempting to generate." 
+  end
+
+  test "A zipped output folder can be used to train YOLOV2" do
+    # Extract zipped folder
+    # Launch YOLOV2 training process
+    # Check that training does not fail with error
+    assert false, "Test not implemented"
   end
 
 end
