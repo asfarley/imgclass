9cba9d70e5620f821379bbc43118368c8579ec11
Ubuntu <ubuntu@ip-172-31-22-75.us-west-2.compute.internal>
2017-10-14 19:52:01 +0000

    Fixing install script - don't open new bash terminals

--- installRails.sh
+++ installRails.sh
@@ -1,15 +1,15 @@
-sudo apt-get update
-sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs
+sudo apt-get update -y
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs -y
 
 cd
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-exec $SHELL
+source ~/.bashrc
 
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-exec $SHELL
+source ~/.bashrc
 
 rbenv install 2.4.1
 rbenv global 2.4.1
@@ -28,3 +28,5 @@ rbenv rehash
 
 rails -v
 # Rails 5.1.3
+
+bundle install
