a5683a043a36ffbbaf9db9cb43729c0b57fd2aa4
Alexander Farley <alexander.s.farley@gmail.com>
2017-10-15 11:38:35 -0600

    Trying to fix Gemfile.lock for ElasticBeanstalk deployment: recreate it on Ubuntu

--- installRails.sh
+++ installRails.sh
@@ -1,5 +1,5 @@
 sudo apt-get update -y
-sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs -y
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs libpq-dev -y
 
 cd
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
@@ -29,6 +29,7 @@ rbenv rehash
 rails -v
 # Rails 5.1.3
 
+mkdir /srv/imgclass
 cd /srv/imgclass
 bundle install
 
