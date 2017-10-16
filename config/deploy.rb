e24de9ee9d11dfc1f5bed0c08d12f10cad2794dc
Alexander Farley <alexander.s.farley@gmail.com>
2017-10-15 21:19:40 -0600

    Removing unnecessary symlink

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,6 @@ set :repo_url, 'https://github.com/asfarley/imgclass.git' # Edit this to match y
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
-append :linked_files, "db/production.sqlite3"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
