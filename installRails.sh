e7b30a7386aca95a7a398ee4e0932585ce0db5e3
Alexander Farley <alexander.s.farley@gmail.com>
2017-10-15 19:45:53 -0600

    Nginx setup

--- installRails.sh
+++ installRails.sh
@@ -1,3 +1,7 @@
+sudo adduser deploy
+sudo adduser deploy sudo
+su deploy
+
 sudo apt-get update -y
 sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs libpq-dev nginx -y
 
@@ -26,18 +30,17 @@ gem install rails -v 5.1.3
 
 rbenv rehash
 
-rails -v
-# Rails 5.1.3
-
-#mkdir /srv/imgclass
-#cd /srv/imgclass
-#bundle install
-
-#rails db:create
-#rails db:schema:load
+rails -v # Rails 5.1.3
 
 git clone git://github.com/dcarley/rbenv-sudo.git ~/.rbenv/plugins/rbenv-sudo
 
-#chmod +x ./launch.sh
+sudo apt-get install -y dirmngr gnupg
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
+sudo apt-get install -y apt-transport-https ca-certificates
 
-#sh ./launch.sh
+sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list'
+sudo apt-get update -y
+sudo apt-get install -y nginx-extras passenger
+sudo sed -i 's|#include /etc/nginx/passenger.conf|include /etc/nginx/passenger.conf|' /etc/nginx/nginx.conf
+sudp cp ./accessory/imgclass.conf /etc/nginx/sites-enabled/
+sudo service nginx restart
