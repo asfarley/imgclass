763dac13f326e52aa40a8e54ae9565a525b16882
Alexander Farley <alexander.s.farley@gmail.com>
2017-10-17 00:01:38 -0600

    Adding script to pull database from production server for backup

--- backup_remote.sh
+++ backup_remote.sh
@@ -3,5 +3,6 @@
 # Meant to be executed in the backup environment, not on the production server itself.
 
 UUID=`uuidgen`
-mkdir -p ~/Backup
-rsync -avz -e "ssh -i ~/.ssh/shared.pem" deploy@ec2-52-42-95-193.us-west-2.compute.amazonaws.com:/var/db/production.sqlite3 ~/Backup/production.sqlite3_backup_$UUID
+BACKUP_PATH=/tmp/backup
+mkdir -p $BACKUP_PATH
+rsync -avz -e "ssh -i ~/.ssh/shared.pem" deploy@ec2-52-42-95-193.us-west-2.compute.amazonaws.com:/var/db/production.sqlite3 $BACKUP_PATH/production.sqlite3_backup_$UUID
