b9f72430bcdf30a6dd05ec59b09777448297cae0
Alexander Farley <alexander.s.farley@gmail.com>
2017-10-20 20:28:46 -0600

    Exposing functionality for editing user roles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,27 @@ class UsersController < ApplicationController
 
   #Show all users
   def index
+    @users = User.all
   end
 
   #Show a particular user
   def show
   end
 
+  def edit
+  end
+
   #Update a particular user's properties
   def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_path, notice: 'User roles were successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   #Destroy a user
