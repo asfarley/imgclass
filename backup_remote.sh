7badeebbd15673ee23fc458472ec9dba170cae61
Alexander Farley <alexander.s.farley@gmail.com>
2017-10-22 23:05:50 -0600

    Simplifying landing page

--- backup_remote.sh
+++ backup_remote.sh
@@ -4,7 +4,7 @@
 
 UUID=`uuidgen`
 USER=deploy
-URL=ec2-35-166-43-90.us-west-2.compute.amazonaws.com
+URL=${SERVER_URL}
 BACKUP_PATH=/tmp/backup
 mkdir -p $BACKUP_PATH
 rsync -avz -e "ssh -i ~/.ssh/shared.pem" $USER@$URL:/var/db/production.sqlite3 $BACKUP_PATH/production.sqlite3_backup_$UUID
