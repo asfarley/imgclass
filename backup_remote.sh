039828cae75c5320412567a94f30b0b6bfca32c7
Alexander Farley <alexander.s.farley@gmail.com>
2017-10-28 15:15:09 -0600

    Small change to rsync command-line flags

--- backup_remote.sh
+++ backup_remote.sh
@@ -4,7 +4,7 @@
 
 UUID=`uuidgen`
 USER=deploy
-URL=${SERVER_URL}
+URL=${IMGCLASS_SERVER_URL}
 BACKUP_PATH=/tmp/backup
 mkdir -p $BACKUP_PATH
 rsync -avz -e "ssh -i ~/.ssh/shared.pem" $USER@$URL:/var/db/production.sqlite3 $BACKUP_PATH/production.sqlite3_backup_$UUID
